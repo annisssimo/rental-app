47c0d4e566e100a7cf9594679445d81feada86e5
Anna <anyalar04@gmail.com>
2024-12-27 17:41:55 +0300

    feat: add prisma client

--- backend/controllers/typePremiseController.js
+++ backend/controllers/typePremiseController.js
@@ -1,35 +1,37 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 const authenticateToken = require('../services/tokenService');
 const typePremiseDb = require('../repositories/typePremiseDb');
 
 module.exports = {
-    async getTypesPremise(req, res) {
-        try {
-            const userData = authenticateToken(req, res);
-            if (!userData) return res.status(401).send();
-    
-            return res.status(200).send(await typePremiseDb.findAllTypesPremises());
-        } catch (error) {
-            console.error('Ошибка при получении типов помещений:', error);
-            return res.status(500).send('Internal Server Error');
-        }
-    },
-    
-    async getTypePremise (req, res) {
-        try {
-            const userData = authenticateToken(req, res);
-            if (!userData) return res.status(401).send();
+  async getTypesPremise(req, res) {
+    try {
+      const userData = authenticateToken(req, res);
+      if (!userData) return res.status(401).send();
 
-            return res.status(200).send(
-                await prisma.typePremises.findUnique({
-                    where: {
-                        ID: parseInt(req.query.id),
-                    },
-                })
-            );
-        } catch (error) {
-            console.error('Ошибка при получении типа помещения:', error);
-            return res.status(500).send('Internal Server Error');
-        }
+      return res.status(200).send(await typePremiseDb.findAllTypesPremises());
+    } catch (error) {
+      console.error('Ошибка при получении типов помещений:', error);
+      return res.status(500).send('Internal Server Error');
     }
-}
+  },
 
+  async getTypePremise(req, res) {
+    try {
+      const userData = authenticateToken(req, res);
+      if (!userData) return res.status(401).send();
+
+      return res.status(200).send(
+        await prisma.typePremises.findUnique({
+          where: {
+            ID: parseInt(req.query.id),
+          },
+        })
+      );
+    } catch (error) {
+      console.error('Ошибка при получении типа помещения:', error);
+      return res.status(500).send('Internal Server Error');
+    }
+  },
+};
